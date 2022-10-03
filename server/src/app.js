de1aea47939ea11a761ab210a284c091f9c9b0bd
Muhammad Asif <muhammadasifhaqnawaz2019@gmail.com>
2022-10-03 17:03:53 +0500

    Initial commit

--- server/src/app.js
+++ server/src/app.js
@@ -3,20 +3,23 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetRouter = require('./routes/planets/planets.router');
+const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
-app.use(morgan('common'));
+app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use(planetRouter);
-app.use('/', (req, res) => {
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
+
+app.use('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 })
 
